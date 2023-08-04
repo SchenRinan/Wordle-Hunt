ebbaceb050f1db3667ef6721424c72ef419996f9
Rinan <jandrewschnell@gmail.com>
2023-08-04 13:51:07 +0200

    folder reorder nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,14 +2,14 @@ import Aboutmodal from "./Aboutmodal";
 import Codesoffcanvas from "./Codesoffcanvas";
 import Creditsmodal from "./Creditsmodal";
 import Loginmodal from "./Loginmodal";
-import Navmain from "./Navmain";
+import Basefeatures from "./Basefeatures";
 import Signupoffcanvas from "./Signupoffcanvas";
 
 const Nav = () => {
     return (
 <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <Navmain/>
+        <Basefeatures/>
         <Aboutmodal/>
         <Creditsmodal/>
         <Loginmodal/>
